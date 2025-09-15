c0ce6092daa6d48d158eea1c05300b8c106dd3c2
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2025-09-15 05:25:20 -0600

    Initial Commit

---
+++ TravelGuide/TravelGuideApp.swift
@@ -0,0 +1,33 @@
+//
+//  TravelGuideApp.swift
+//  TravelGuide
+//
+//  Created by Sebastian Rosas Maciel on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelGuideApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
