e1aafc548e4a0a483d52b2326a9e743b65efdcec
presnosofttek <sebastian.presno@softtek.com>
2025-09-16 18:51:39 -0600

    fixed view Builder helper that got lost in merge

--- FlightStatusWidget/Views/FlightDetailsSmallWidgetView.swift
+++ FlightStatusWidget/Views/FlightDetailsSmallWidgetView.swift
@@ -79,14 +79,3 @@ struct SmallFlightWidgetView: View {
         }
     }
 }
-
-// Helper extension for conditional view modifiers
-extension View {
-    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
-        if condition {
-            transform(self)
-        } else {
-            self
-        }
-    }
-}
--- FlightStatusWidget/Views/SwiftUIView.swift
+++ FlightStatusWidget/Views/SwiftUIView.swift
@@ -105,3 +105,15 @@ struct FlightDetailsMediumWidgetView: View {
         }
     }
 }
+
+extension View {
+    /// Conditionally applies a transformation to a view
+    @ViewBuilder
+    func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
